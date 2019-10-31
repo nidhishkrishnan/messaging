487cea53123fb267adbd2494c263aaf4eadefc07
nidhishkrishnan <nidhishkrishnan@gmail.com>
2019-10-31 21:32:24 +0100

    Initial Commit

---
+++ src/test/java/com/amqp/messaging/MessagingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amqp.messaging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
